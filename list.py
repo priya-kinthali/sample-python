17594c7021a3267a3516f1f505ad5ce45f50f699
priya-kinthali <priya-kinthali@github.com>
2024-03-20 16:06:09 +0530

    826-initial commit

--- list.py
+++ list.py
@@ -6,4 +6,4 @@ names = ["Alice", "Bob", "Charlie"]
 ages = [10, 20, 30]
 
 greet_all(names)   # Ok!
-greet_all(ages)    # Error due to incompatible types
+greet_all(ages)    # Error due to incompatible types testing 
