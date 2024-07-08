6a624722bde0cacdeff3d3e388b0da65c3e0ae14
priya-kinthali <priya-kinthali@github.com>
2024-07-08 18:05:25 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, __version__, __version_tuple__, release
  
 setup(
     name='samplepython02',
-    version='1.0',    
+    version=__version__,    
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages()
---
+++ version.py
@@ -0,0 +1,3 @@
+__version__ = '1.0'
+release = '1.0'
+__version_tuple__ = (1, 0)
