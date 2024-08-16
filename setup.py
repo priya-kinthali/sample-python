7f4a86793553c3bde5f8fb21251827cd878d03e1
priya-kinthali <priya-kinthali@github.com>
2024-08-16 13:44:34 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='samplepython02',
-    version='1.1',    
+    version='1.0.0',    
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages()
