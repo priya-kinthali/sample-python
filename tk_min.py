696c544775273f27b103a06387ad371a24a9eb8a
priya-kinthali <priya-kinthali@github.com>
2024-10-30 18:49:17 +0530

    with removed code

--- tk_min.py
+++ tk_min.py
@@ -5,15 +5,3 @@ label = tk.Label(root, text="Hello World!")
 label.pack(padx=20, pady=20)
 root.after(5000, root.destroy)    # Close the Window after 5 seconds
 root.mainloop()
-
-
-import tkinter
-import _tkinter
-
-header = _tkinter.TK_VERSION
-lib = tkinter.Tk().getvar('tk_version')
-
-if lib != header:
-  print('header version=' + header)
-  print('lib version=' + lib)
-  exit(1)
