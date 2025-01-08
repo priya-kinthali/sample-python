2d9bf4b5bf537cc2bd7b706625cad577b1f1b923
priya-kinthali <priya-kinthali@github.com>
2025-01-08 15:37:49 +0530

    with freethreaded

--- thread.py
+++ thread.py
@@ -1,31 +1,33 @@
 import threading
 import time
 
-def print_cube(num):
-    print("Thread for cube started")
-    time.sleep(1)  # Simulate a time-consuming task
-    print("Cube: {}".format(num * num * num))
-    print("Thread for cube finished")
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
 
-def print_square(num):
-    print("Thread for square started")
-    time.sleep(1)  # Simulate a time-consuming task
-    print("Square: {}".format(num * num))
-    print("Thread for square finished")
+def print_fib(num):
+    print(f"Thread for Fibonacci({num}) started")
+    result = fib(num)
+    print(f"Fibonacci({num}): {result}")
+    print(f"Thread for Fibonacci({num}) finished")
 
 if __name__ == "__main__":
     print("Main thread started")
     
-    t1 = threading.Thread(target=print_square, args=(10,))
-    t2 = threading.Thread(target=print_cube, args=(10,))
+    t1 = threading.Thread(target=print_fib, args=(35,))
+    t2 = threading.Thread(target=print_fib, args=(35,))
 
     print("Starting threads")
+    start_time = time.time()
     t1.start()
     t2.start()
 
     print("Waiting for threads to complete")
     t1.join()
     t2.join()
+    end_time = time.time()
 
     print("All threads completed")
-    print("Done!")
+    print(f"Done in {end_time - start_time:.2f} seconds!")
