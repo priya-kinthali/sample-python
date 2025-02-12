179d1a8282fe2ea006ce3ee2493609e9129e1ba9
priya-kinthali <priya-kinthali@github.com>
2025-02-12 12:36:58 +0530

    Test Permoramnce for hello world

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+import time
+
+start_time = time.time()
+
+print("Hello, World!")
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(f"Execution time: {execution_time} seconds")
